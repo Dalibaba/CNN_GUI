f06e8c42ef82ee11d2a5a3d7125fd487a4d82be8
Dalibaba <linkedavi@googlemail.com>
2021-07-17 10:47:27 +0200

    two screens added in stacked layout

--- constants.py
+++ constants.py
@@ -1,6 +1,26 @@
-CHOOSE_IMAGE_LABEL = "Choose Image"
-IMAGE_CLASSIFICATION_LABEL = "Classify Images with Model"
-CLASSIFY_IMAGE_LABEL = "Classify"
 
-MAIN_SCREEN_WIDTH = 800
-MAIN_SCREEN_HEIGHT = 800
+
+class WINDOW:
+    TITLE_LABEL = "IMAGE CLASSIFICATION - Train and Classify"
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 800
+    CLASSIFY_SCREEN_INDEX = 1
+    TRAIN_SCREEN_INDEX = 0
+
+class MENUBAR:
+    EXIT_LABEL = '&Exit'
+    TRAIN_LABEL = '&Train CNN'
+    CLASSIFY_LABEL = '&Classify with CNN'
+    SCREEN_LABEL = '&Classify with CNN'
+
+class CLASSIFY_WIDGET:
+    CHOOSE_LABEL = "Choose Image"
+    CLASSIFICATION_LABEL = "Classify Images with Model"
+    CLASSIFY_LABEL = "Classify Images with Model"
+class TRAIN_WIDGET:
+    TRAIN_LABEL = "Choose Image"
+    CLASSIFY_IMAGE_LABEL = "Classify Images with Model"
+    RESNET_LABEL = "ResNet-50"
+    VGG16_LABEL = "VGG16"
+    MODEL_TYPE = "Type of Model"
+
--- main.py
+++ main.py
@@ -1,68 +1,107 @@
-from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QTextEdit, QAction, \
+    QMainWindow, QStackedWidget, QFormLayout, QHBoxLayout, QRadioButton, QLineEdit
 import sys
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 
+from constants import WINDOW, CLASSIFY_WIDGET, TRAIN_WIDGET
+import menubar
 
-import constants
-
-
-class Window(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        # labels
-        self.image_label = QLabel("")
-        self.image_classification_label = QLabel(constants.IMAGE_CLASSIFICATION_LABEL)
-        # buttons
-        self.classify_image_button = QPushButton(constants.CLASSIFY_IMAGE_LABEL)
-        self.choose_image_button = QPushButton(constants.CHOOSE_IMAGE_LABEL)
+        QMainWindow.__init__(self)
+        cnn_menubar_train_action, cnn_menubar_classify_action = menubar.init_menubar(self)
+        # A QMainWindow must have a central widget to display correctly
+        self.central_widget = QWidget()  # define central widget
+        self.setCentralWidget(self.central_widget)  # set QMainWindow.centralWidget
 
-        self.title = "CNN_CLASSIFIER"
+        # Main Window Configurations
         self.top = 200
         self.left = 500
-        self.width = constants.MAIN_SCREEN_WIDTH
-        self.height = constants.MAIN_SCREEN_HEIGHT
-
-        self.init_window()
-
-    def init_window(self):
+        self.title = WINDOW.TITLE_LABEL
+        self.width = WINDOW.SCREEN_WIDTH
+        self.height = WINDOW.SCREEN_HEIGHT
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # Initialize two pages with stacked widget
+        # child widgets
+        self.train_widget_stack = QWidget()
+        self.classify_widget_stack = QWidget()
+        self.train_widget()
+        self.classify_widget()
+        # add child widged to parent widget (stacked)
+        self.stack_widget = QStackedWidget(self)
+        self.stack_widget.addWidget(self.train_widget_stack)
+        self.stack_widget.addWidget(self.classify_widget_stack)
+
+        vbox = QVBoxLayout()
+        self.centralWidget().setLayout(vbox)  # add the layout to the central widget
+        # add trigger events clicking the menu bar
+        cnn_menubar_train_action.triggered.connect(self.display_train_widget)
+        cnn_menubar_classify_action.triggered.connect(self.display_classify_widget)
+        # self.cnn_menubar.currentRowChanged.connect(self.display)
+
+        vbox.addWidget(self.stack_widget)
+        self.show()
+
+    def classify_widget(self):
+        vbox = QVBoxLayout()
+        # labels
+        self.image_label = QLabel("")
+        image_classification_label = QLabel(CLASSIFY_WIDGET.CLASSIFICATION_LABEL)
+        # buttons
+        classify_image_button = QPushButton(CLASSIFY_WIDGET.CLASSIFY_LABEL)
+        choose_image_button = QPushButton(CLASSIFY_WIDGET.CHOOSE_LABEL)
         # alignment
-        self.image_classification_label.setAlignment(Qt.AlignCenter)
+        image_classification_label.setAlignment(Qt.AlignCenter)
         self.image_label.setAlignment(Qt.AlignCenter)
         # Create a QVBoxLayout instance
-        vbox = QVBoxLayout()
         # add widgets to vbox
-        vbox.addWidget(self.image_classification_label)
-        vbox.addWidget(self.choose_image_button)
+        vbox.addWidget(image_classification_label)
+        vbox.addWidget(choose_image_button)
         vbox.addWidget(self.image_label)
-        vbox.addWidget(self.classify_image_button)
+        vbox.addWidget(classify_image_button)
 
         # click events
-        self.choose_image_button.clicked.connect(self.get_image)
+        choose_image_button.clicked.connect(self.get_image)
 
-        self.setLayout(vbox)
+        # self.setLayout(vbox)
+        self.classify_widget_stack.setLayout(vbox)
 
-        self.show()
+    def train_widget(self):
+
+        train_label = QLabel(TRAIN_WIDGET.TRAIN_LABEL)
+        # form layout for defining parameters
+        form_layout = QFormLayout()
+        model_layout = QHBoxLayout()
+        model_layout.addWidget(QRadioButton(TRAIN_WIDGET.RESNET_LABEL))
+        model_layout.addWidget(QRadioButton(TRAIN_WIDGET.VGG16_LABEL))
+        form_layout.addRow(train_label)
+        form_layout.addRow(QLabel(TRAIN_WIDGET.MODEL_TYPE), model_layout)
+
+
+        self.train_widget_stack.setLayout(form_layout)
+
+    def display_classify_widget(self):
+        self.stack_widget.setCurrentIndex(WINDOW.CLASSIFY_SCREEN_INDEX)
+
+    def display_train_widget(self):
+        self.stack_widget.setCurrentIndex(WINDOW.TRAIN_SCREEN_INDEX)
 
     def get_image(self):
         fileName = QFileDialog.getOpenFileName(self,
                                                "Open Image", "/home/jana", "Image Files (*.png *.jpg *.bmp)")
         imagePath = fileName[0]
-        print("hi")
         pixmap = QPixmap(imagePath)
-        image_height = int(constants.MAIN_SCREEN_HEIGHT * 0.5)
-        image_width = int(constants.MAIN_SCREEN_WIDTH * 0.5)
+        image_height = int(WINDOW.SCREEN_HEIGHT * 0.5)
+        image_width = int(WINDOW.SCREEN_WIDTH * 0.5)
         pixmap = pixmap.scaled(image_height, image_width)
-
-        # self.image_label.setScaledContents(self, True)
+        print("set image")
         self.image_label.setPixmap(QPixmap(pixmap))
-        # self.resize(image_height, image_width)
 
 
 if __name__ == "__main__":
     App = QApplication(sys.argv)
-    window = Window()
+    window = MainWindow()
     sys.exit(App.exec())
